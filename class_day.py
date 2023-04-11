f8a73e61d4ee194e40eabce2b6a25df93de63988
avglrd <skublll@mail.ru>
2023-04-12 00:24:19 +0300

    разработка модуля составление расписания

--- class_day.py
+++ class_day.py
@@ -1,9 +1,11 @@
 class Day:
     def __init__(self, start_time: str, duration):
+        self.duration = duration
+        self.start_time = start_time
         self.mass_time = []
         duration = float(duration)/60
         start = float(start_time[-2:]) / 60 + float(start_time[:-3])
         i = 0
         while start + float(duration) * i < 24:
-            self.mass_time.append(i)
+            self.mass_time.append(start + duration * i)
             i += 1
--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Fields:
 
 
 class Games:
-    def __init__(self, team1, team2, time):
+    def __init__(self, team1, team2, time: str):
         self.team1 = team1
         self.team2 = team2
         self.time = time
@@ -46,17 +46,14 @@ for i in range(len(days_db)):
     field = Fields(k, day)
     days.append(field)
 
-
 mass_games = []
 
 stadiums = []
-
 
 for i in range(len(days_db)):
     stadiums.append(days_db[i].stadium)
 
 stadiums = list(set(stadiums))
-
 
 for i in range(len(stadiums)):
     f = stadiums[i]
@@ -69,19 +66,40 @@ for i in range(len(stadiums)):
     '''print(f)
     print(":")
     for k in range(len(s)):
-        print(s[k].team.name)
-    '''
-    use_fields = [] #массив days, отсортированный по стадиону
+        print(s[k].team.name)'''
+
+    use_fields = []  # массив days, отсортированный по стадиону
     for k in range(len(days)):
         if days[k].field.stadium == f:
             use_fields.append(days[k])
-
-
-
-
-
-
-
-
-
+    '''print(use_fields[0].field.start_time, use_fields[0].field.duration)
+    print(s[0].team.time_wish)
+    for p in range(len(s)):
+        print(s[p].team.name)
+        print(s[p].team.time_wish)'''
+    print(use_fields[0].day.mass_time)
+    while len(s) > 1:
+        for q in range(len(s) - 1):
+            n_left = int(s[0].team.time_wish[:2])
+            n_right = int(s[0].team.time_wish[6:-3])
+            m_left = int(s[q + 1].team.time_wish[:2])
+            m_right = int(s[q + 1].team.time_wish[6:-3])
+            if m_left <= n_left <= m_right or m_left <= n_right <= m_right or n_left <= m_right <= n_right or n_left <= m_left <= n_right:
+                if m_left <= n_left <= m_right: # skoree vsego nuzhno budet dobavit eshe odno uslovie o granicah s dregih storon
+                    for count in range(len(use_fields)):
+                        for durat in range(len(use_fields[count].day.mass_time) - 1):
+                            if n_left <= use_fields[count].day.mass_time[durat] <= m_right and use_fields[count].day.mass_time[durat+1] - use_fields[count].day.mass_time[durat] == 1.5:
+                                k = Games(s[0], s[q+1], str(use_fields[count].day.mass_time[durat]))
+                                mass_games.append(k)
+                                print(s[0].team.name, s[q + 1].team.name, use_fields[count].day.mass_time[durat])
+                                s.pop(0)
+                                s.pop(q)
+                                '''
+                                print("\n")
+                                for inn in range(len(s)):
+                                    print(s[inn].team.name)
+                                print("\n")
+                                '''
+                                use_fields[count].day.mass_time.pop(durat)
+        break
 
